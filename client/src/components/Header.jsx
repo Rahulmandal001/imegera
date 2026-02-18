fb1b922a619aa296db114fd00c188c2bce02af8c
rahul mandal001 <therahulmandal001@gmail.com>
2026-02-18 22:36:54 +0530

    Added footer credit and UI improvements

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
         animate={{opacity:1, y:0}}
         transition={{delay: 0.2, duration:0.8}}
         >
-        <p>Best text to image generator</p>
+        <p>Developed by Rahul Mandal</p>
         <img src={assets.star_icon} alt="" />
       </motion.div>
       <motion.h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>Turn Text To <span className='text-blue-600'
